7286fb922586010b7e597556bf9ae30446bfa3af
unknown <sifatniloy18@gmail.com>
2022-12-15 05:10:14 -0600

    inheitance , vehicle class

--- Files & Exception/exception_handle.py
+++ Files & Exception/exception_handle.py
@@ -3,6 +3,9 @@ def div(a,b):
         return a/b
     except ZeroDivisionError:
         print("Can not divide by zero")
+    except TypeError:
+        print("Unsupported type. Did you use string? ")
 print(div(10,2))
 print(div(3,0))
 print(div(9,3))
+print(div("12",3))
